2581d767f687a3aa7a7c30880264adaac9880caa
Shashank S Shetty <smile.shashank.s.shetty1996@gmail.com>
2019-07-27 11:01:51 +0530

    Fix store issue

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 import { createStore, compose } from 'redux';
 
-import rootReducers from './store/rootReducers';
+import rootReducers from './rootReducers';
 
 const composeEnhancers =
   process.env.NODE_ENV === 'development'
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
     : null || compose;
 
-const store = createStore(rootReducers, composeEnhancers);
+const store = createStore(rootReducers, composeEnhancers());
 
 export default store;
