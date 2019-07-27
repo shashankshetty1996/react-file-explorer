fe1cff2f9d025ad3e10f62942bc3563c88953a10
Shashank S Shetty <smile.shashank.s.shetty1996@gmail.com>
2019-07-27 20:56:27 +0530

    Add route based data rendering

--- src/container/Content/Content.js
+++ src/container/Content/Content.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 import { AddField, Thumbnail } from '../../components';
+import { getPathArray } from '../../utils/utils';
 
 import './Content.scss';
 
 const Content = props => {
-  const { root } = props.directory;
+  // const { root } = props.directory;
+  const {
+    directory: { root },
+    location: { pathname },
+  } = props;
 
   // Method to add file to directory store.
   const addFile = () => {};
 
-  // const data = [...root];
+  const path = getPathArray(pathname);
+  const data = path.slice(1).reduce(
+    (acc, name) => {
+      const res = acc.find(el => el.name === name);
+      return res.children;
+    },
+    [...root]
+  );
 
   return (
     <section className="content">
-      {root.map(el => (
+      {data.map(el => (
         <Thumbnail className="icon" key={el.id} isDirectory={el.is_directory} name={el.name} />
       ))}
       <AddField onClick={addFile} />
@@ -26,10 +39,11 @@ const Content = props => {
 
 Content.propTypes = {
   directory: PropTypes.object.isRequired,
+  location: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = state => ({
   directory: state.directory,
 });
 
-export default connect(mapStateToProps)(Content);
+export default withRouter(connect(mapStateToProps)(Content));
