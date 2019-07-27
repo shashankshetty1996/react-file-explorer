3a6f97879d9dc4ec88c28359925ae7fdda41dcb6
Shashank S Shetty <smile.shashank.s.shetty1996@gmail.com>
2019-07-27 11:05:47 +0530

    Dummy Directory reducer

--- src/store/reducers/Directory.reducer.js
+++ src/store/reducers/Directory.reducer.js
@@ -1,1 +1,7 @@
-// Directory Reducer file here
+import { Directory as initialState } from '../meta/initialState/index';
+
+const Directory = (state = initialState, action = {}) => {
+  return state;
+};
+
+export default Directory;
