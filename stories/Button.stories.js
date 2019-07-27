9f86ca2ff21091bac27e843ce3bdc118a50d5b5a
Shashank S Shetty <smile.shashank.s.shetty1996@gmail.com>
2019-07-27 15:27:17 +0530

    Button story

---
+++ stories/Button.stories.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { Button } from '../src/components';
+
+import '../src/components/Button/Button.scss';
+
+import '../src/style/_main.scss';
+
+const styles = {
+  background: 'rgb(245,247,249)',
+  height: '70vh',
+  padding: '24px',
+  width: '250px',
+};
+
+storiesOf('Button', module).add('Basic Button', () => {
+  return (
+    <div style={styles}>
+      <Button onClick={() => console.log('close clicked')}>Create</Button>
+    </div>
+  );
+});
