e386636ac3edc9b7ff65b7ed81370b99ba62b5b8
Shashank S Shetty <smile.shashank.s.shetty1996@gmail.com>
2019-07-28 20:21:55 +0530

    Fix clear info action

--- src/store/actionTypes.js
+++ src/store/actionTypes.js
@@ -3,7 +3,7 @@ const actionTypes = {
     OPEN: 'SUB_MENU-OPEN',
     GET_INFO: 'SUB_MENU-GET_INFO',
     DELETE: 'SUB_MENU-DELETE',
-    CLEAR_INFO: 'SUB_MENU-GET_INFO',
+    CLEAR_INFO: 'SUB_MENU-CLEAR_INFO',
   },
 };
 
