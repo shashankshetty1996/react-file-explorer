9cc8e6658f6a8f84632226e553bec3d35c1fee50
Shashank S Shetty <smile.shashank.s.shetty1996@gmail.com>
2019-07-28 16:17:42 +0530

    Content section clean up

--- src/container/Content/Content.js
+++ src/container/Content/Content.js
@@ -1,30 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
 
 import { AddField, Thumbnail } from '../../components';
-import { getPathArray } from '../../utils/utils';
 
 import './Content.scss';
 
 const Content = props => {
-  const {
-    directory: { root },
-    location: { pathname },
-  } = props;
+  const { data } = props;
 
   // Method to add file to directory store.
   const addFile = () => {};
-
-  const path = getPathArray(pathname);
-  const data = path.slice(1).reduce(
-    (acc, name) => {
-      const res = acc.find(el => el.name === name);
-      return res ? res.children : [];
-    },
-    [...root]
-  );
 
   const onSubMenuClickHandler = (id, action) => {};
 
@@ -44,12 +29,7 @@ const Content = props => {
 };
 
 Content.propTypes = {
-  directory: PropTypes.object.isRequired,
-  location: PropTypes.object.isRequired,
+  data: PropTypes.array.isRequired,
 };
 
-const mapStateToProps = state => ({
-  directory: state.directory,
-});
-
-export default withRouter(connect(mapStateToProps)(Content));
+export default Content;
--- src/layout/MainLayout/MainLayout.js
+++ src/layout/MainLayout/MainLayout.js
@@ -1,19 +1,49 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 import { Content, Header, SideNav } from '../../container';
 
+import { getPathArray, prepareDirectoryTree } from '../../utils/utils';
+
 import './MainLayout.scss';
 
-const MainLayout = () => {
+const MainLayout = props => {
+  const {
+    directory: { root },
+    location: { pathname },
+  } = props;
+
+  const directoryTree = prepareDirectoryTree(root);
+
+  const path = getPathArray(pathname);
+  const data = path.slice(1).reduce(
+    (acc, name) => {
+      const res = acc.find(el => el.name === name);
+      return res ? res.children : [];
+    },
+    [...root]
+  );
+
   return (
     <div className="main-layout">
-      <SideNav />
+      <SideNav data={directoryTree} />
       <main>
         <Header />
-        <Content />
+        <Content data={data} />
       </main>
     </div>
   );
 };
 
-export default MainLayout;
+MainLayout.propTypes = {
+  directory: PropTypes.object.isRequired,
+  location: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+  directory: state.directory,
+});
+
+export default withRouter(connect(mapStateToProps)(MainLayout));
