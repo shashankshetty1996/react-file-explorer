4c96cbac3865c3027d7ae0f8bd43af785772bace
Shashank S Shetty <smile.shashank.s.shetty1996@gmail.com>
2019-07-28 16:20:56 +0530

    Prepare Directory tree method

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,4 +4,18 @@ const getPathArray = pathname => (pathname === '/' ? [''] : pathname.split('/'))
 
 const getParentDirectory = pathname => pathname.substring(0, pathname.lastIndexOf('/'));
 
-export { getFileExtension, getPathArray, getParentDirectory };
+const prepareDirectoryTree = data => {
+  return data
+    .filter(file => {
+      return file.is_directory;
+    })
+    .map(item => {
+      let { children } = item;
+      if (children.length) {
+        children = prepareDirectoryTree(children);
+      }
+      return { id: item.id, name: item.name, children: [...children] };
+    });
+};
+
+export { getFileExtension, getPathArray, getParentDirectory, prepareDirectoryTree };
