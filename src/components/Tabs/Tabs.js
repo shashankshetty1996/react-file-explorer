06286652180a3643eee9ce0845c5a1fcb54965f1
Shashank S Shetty <smile.shashank.s.shetty1996@gmail.com>
2019-08-04 18:04:12 +0530

    Add Tab scss fix

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
+import './Tabs.scss';
+
 const Tabs = props => {
   const { tabs, onTabClick, defaultSelected, className } = props;
 
