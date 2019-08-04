eb8fe80148aed1a3db4a4e0974bc6d3e3df47a01
Shashank S Shetty <smile.shashank.s.shetty1996@gmail.com>
2019-08-04 18:43:38 +0530

    Updated constants file with content type fields

--- src/Constants.js
+++ src/Constants.js
@@ -4,6 +4,17 @@ const Constants = {
     GET_INFO: 'Get info',
     DELETE: 'Delete',
   },
+  CONTENT_TYPE: {
+    TYPE: {
+      FILE: 'FILE',
+      FOLDER: 'FOLDER',
+    },
+    ACTION: {
+      SET_TYPE: 'SET_TYPE',
+      SET_INPUT: 'SET_INPUT',
+      CLEAR_INPUT: 'CLEAR_INPUT',
+    },
+  },
 };
 
 export default Constants;
